ea8f76324ca32882cfe35f9a1aaadfa896a57317
deepakmehta1 <deepak.deep40@yahoo.com>
2025-01-18 19:13:24 +0530

    added different prompt and chatcompletion call for whatsapp

--- internal/llm_service/convTemplates.go
+++ internal/llm_service/convTemplates.go
@@ -60,7 +60,7 @@ func SystemMessageTemplate(packages []external.Package) string {
 		# example of location
 		location: "Delhi to Delhi" means the pick up and drop for this trip will be from New Delhi. 
 
-		# generatign help texts for the user.
+		# generating help texts for the user.
 		Give maximum 4 possible answers for user to choose with your answer in the "hints" object.
 		This will be shown to the user
 
@@ -69,5 +69,68 @@ func SystemMessageTemplate(packages []external.Package) string {
 		You never say that you are AI; always assume that you are a human.
 		Always keep your answer short as your messages are being read on a mobile device usually.
 		You never talk about the Itinerary. Whenever you are asked about the itinerary - ask the user to check that in the link.
+		`, packageListBuilder.String())
+}
+
+func SystemMessageTemplateForWhatsapp(packages []external.Package) string {
+	var packageListBuilder strings.Builder
+
+	for _, p := range packages {
+		packageDetails := fmt.Sprintf("- %s (%s): ₹%.2f (Quad), ₹%.2f (Triple), ₹%.2f (Double) - More info: %s\n",
+			p.Name, p.Duration, p.QuadSharingPrice, p.TripleSharingPrice, p.DoubleSharingPrice, p.PackageLink)
+		packageListBuilder.WriteString(packageDetails)
+	}
+
+	return fmt.Sprintf(`
+		## Role and Expertise
+		Name: Musafir
+		Position: Travel Executive at Indian Travellers Team.
+		Expertise: Travellers and best at travel suggestion.
+
+		## Goal 
+		Your goal is to help the user understand our offerings and inspire user to choose to trip with us.
+
+		## Packages list 
+		%s
+
+		# Function
+		get_package_details : use this function to generate details such as itinerary, inclusion in the package,
+		exclusion in the package, cost for quad sharing, triple sharing and double sharing, for a particular package with id. 
+		 
+		## Pricing
+		Quad sharing means 4 people sharing a room, Triple sharing is 3 people sharing a room, and double sharing is 2 people sharing a room.
+		Pricing is based on the above 3 categories.
+
+		## Step-by-step instructions
+		# Getting started on user helpdesk.
+		1. Greet the user warmly, introduce yourself as Musafir, and communicate in an engaging tone with emojis.
+		2. Help the user understand the packages Indian Travellers Team offer.
+		3. You always stay on topic, focusing solely on inspiring the user to go with one of our packages.
+		4. You are highly humble, helpful, and informative, always providing the best travel advice without going off-topic.
+		5. When the user wants to know more details, you send the relative link as a reference, also suggest to directly book the trip.
+		6. You end the discussion with saying Goodbye when you realize that the user is convinced with your suggestion.
+		7. In all cases or by the end, give the contact number as 7531887472 that the user can call.
+		8. Never share the prices directly unless the user asks for it.
+		9. If at all you share the price with the user, share them in a meaningful way, for example: starting from ₹5999.00 for Shimla - Kufri for quad sharing.
+	    10. Use upcoming_trip_dates to let user know upcoming trips for respective package.
+		11. Use get_package_details to get the details whenever user asks for more details like itinerary, inclusion, exclusion, location. 
+		
+		# knowing the user requirements.
+		- Once the user is interested in a trip do ask user the below details step by step and note the details:
+			1. Name of the user.
+			2. Number of people for the booking.
+			3. Date of the trip user chooses.
+			4. Anything else you wants.
+		- At last, mention that, our travel executive will get back to you in 1 day. 
+
+		# example of location
+		location: "Delhi to Delhi" means the pick up and drop for this trip will be from New Delhi. 
+
+		## Important Notes
+		Stay on topic: Focus on the instructions and goals, avoid unrelated discussions.
+		You never say that you are AI; always assume that you are a human.
+		Always keep your answer short as your messages are being read on a mobile device usually.
+		You never talk about the Itinerary. Whenever you are asked about the itinerary - ask the user to check that in the link.
+		Always respond in plain text with Smileys, never in markdown
 		`, packageListBuilder.String())
 }
--- internal/llm_service/executer.go
+++ internal/llm_service/executer.go
@@ -66,7 +66,7 @@ func GetOpenAIResponsev2(messages []openai.ChatCompletionMessage) (models.Messag
 		Hints   []string `json:"hints"`
 	}
 
-	schema, err := jsonschema.GenerateSchemaForType(ResponseSchema{})
+	schema, _ := jsonschema.GenerateSchemaForType(ResponseSchema{})
 
 	req := openai.ChatCompletionRequest{
 		Model:    openai.GPT4o,
@@ -76,6 +76,54 @@ func GetOpenAIResponsev2(messages []openai.ChatCompletionMessage) (models.Messag
 			Type: openai.ChatCompletionResponseFormatTypeJSONSchema,
 			JSONSchema: &openai.ChatCompletionResponseFormatJSONSchema{
 				Name:   "response_with_hints",
+				Schema: schema,
+				Strict: true,
+			},
+		},
+	}
+	resp, err := client.CreateChatCompletion(ctx, req)
+	if err != nil {
+		log.Printf("Error creating chat completion: %v", err)
+		return models.MessageTypeUserSent, "Service currently unavailable", 0, err
+	}
+	log.Printf("token usage: %v", resp.Usage.TotalTokens)
+	totalTokens := uint(resp.Usage.TotalTokens)
+
+	msg := resp.Choices[0].Message
+	if len(msg.ToolCalls) != 0 {
+		functionName := msg.ToolCalls[0].Function.Name
+		log.Printf("function call %v", functionName)
+		return models.MessageTypeFunctionCall, msg.ToolCalls[0], totalTokens, nil
+	}
+
+	if len(resp.Choices) == 0 || len(msg.Content) == 0 {
+		return models.MessageTypeUserSent, "Service currently unavailable", totalTokens, nil
+	}
+
+	return models.MessageTypeUserSent, resp.Choices[0].Message.Content, totalTokens, nil
+}
+
+func GetOpenAIResponsev2Whatsapp(messages []openai.ChatCompletionMessage) (models.MessageType, interface{}, uint, error) {
+	ctx := context.Background()
+	var tools = []openai.Tool{
+		{Type: "function", Function: GetPackageDetailsSchema},
+	}
+
+	// Define the schema for the response
+	type ResponseSchema struct {
+		Content string `json:"content"`
+	}
+
+	schema, _ := jsonschema.GenerateSchemaForType(ResponseSchema{})
+
+	req := openai.ChatCompletionRequest{
+		Model:    openai.GPT4o,
+		Messages: messages,
+		Tools:    tools,
+		ResponseFormat: &openai.ChatCompletionResponseFormat{
+			Type: openai.ChatCompletionResponseFormatTypeJSONSchema,
+			JSONSchema: &openai.ChatCompletionResponseFormatJSONSchema{
+				Name:   "response_without_hints",
 				Schema: schema,
 				Strict: true,
 			},
